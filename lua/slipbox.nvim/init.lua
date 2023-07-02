529684b7774e075dbb019cdb9a5fb205b4e4ef1f
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-07-02 18:09:17 -0400

    insert_date function

--- lua/slipbox.nvim/init.lua
+++ lua/slipbox.nvim/init.lua
@@ -1,6 +1,7 @@
 local vim = vim
+local M = {}
 
-function insert_date()
+function M.insert_date()
     local date = os.date('_%Y%m%d%H%M')
     local current_pos = vim.api.nvim_win_get_cursor(0)
     vim.api.nvim_put({date}, "b", true, true)
@@ -8,3 +9,5 @@ function insert_date()
 end
 
 vim.keymap.set('n', '<Leader>il', '<CMD>lua insert_date()<CR>', {noremap=true, silent=true})
+
+return M
