490710c037def8cc75fbf93cdf88c79bde2b960f
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-07-02 18:46:42 -0400

    remove dot from plugin subdir

---
+++ lua/slipbox/init.lua
@@ -0,0 +1,13 @@
+local vim = vim
+local M = {}
+
+--function M.insert_date()
+    --local date = os.date('_%Y%m%d%H%M')
+    --local current_pos = vim.api.nvim_win_get_cursor(0)
+    --vim.api.nvim_put({date}, "b", true, true)
+    --vim.api.nvim_win_set_cursor(0, current_pos)
+--end
+
+print('testing!')
+
+return M
