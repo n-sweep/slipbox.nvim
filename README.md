54d490f95238c37dc31d3a18165454fef5f92f34
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-07-03 07:40:54 -0400

    insert_date function

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A minimal plugin to support the zettelkasten method
 ## TODO:
 
 - [x] function to insert timestamp
-- [ ] verify working via packer / github
+- [x] verify working via packer / github
+- [ ] set keymap w/ plugin
 - [ ] plugin only active in set directory
 - [ ]
--- lua/slipbox/init.lua
+++ lua/slipbox/init.lua
@@ -2,12 +2,12 @@ local vim = vim
 local M = {}
 
 function M.insert_date()
-    local date = os.date('_%Y%m%d%H%M')
+    local date = os.date('%Y%m%d%H%M')
     local current_pos = vim.api.nvim_win_get_cursor(0)
     vim.api.nvim_put({date}, "b", true, true)
     vim.api.nvim_win_set_cursor(0, current_pos)
 end
 
-vim.keymap.set('n', '<Leader>il', '<CMD>lua insert_date()<CR>', {noremap=true, silent=true})
+vim.api.set_keymap.set('n', '<Leader>id', '<CMD>lua require("slipbox").insert_date()<CR>', {noremap=true, silent=true})
 
 return M
